e083f4a82ecd057821dc2efacb8e859ec56cd33c
mKroogz <mattk932009@gmail.com>
2020-06-19 20:55:10 -0500

    made skeleton view of note management

--- binder/views/__init__.py
+++ binder/views/__init__.py
@@ -5,3 +5,6 @@ from .auth.logout import logout_user
 from .auth.reg_form import reg_form
 from .notes.new_note import new_note
 from .notes.existing_note import existing_note, existing_note_pt2
+from .lists.seasons import season_list
+from .lists.classes import class_list
+from .lists.notes import note_list
---
+++ binder/views/lists/notes.py
@@ -0,0 +1,28 @@
+import sqlite3
+from django.shortcuts import render, redirect, reverse
+from binder.models import Note, model_factory
+from ..connection import Connection
+
+def get_notes(school_class):
+    with sqlite3.connect(Connection.db_path) as conn:
+        conn.row_factory = model_factory(Note)
+        db_cursor = conn.cursor()
+
+        db_cursor.execute("""
+        select
+            n.id,
+            n.name
+        from binder_note n
+        WHERE n.school_class_id = ?
+        """, (school_class,))
+
+        return db_cursor.fetchall()
+
+def note_list(request, school_class):
+    if request.method == 'GET':
+        notes = get_notes(school_class)
+        template = 'lists/notes.html'
+        context = {
+            'all_notes': notes
+        }
+        return render(request, template, context)
